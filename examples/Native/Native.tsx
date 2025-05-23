e06817c3109aff094901e1d1156121d96598711f
Bartłomiej Błoniarz <bartlomiej.bloniarz@swmansion.com>
2025-05-23 17:30:29 +0200

    Use rn-svg by default

--- examples/Native/Native.tsx
+++ examples/Native/Native.tsx
@@ -56,8 +56,8 @@ const data = Array.from({length: 60}, (_, i) => ({
 const SVGView = () => {
   return (
     <>
-      {/* <SvgXml xml={svgXmlData} /> */}
-      <Image source={svg} style={styles.image} contentFit="contain" />
+      <SvgXml xml={svgXmlData} />
+      {/* <Image source={svg} style={styles.image} contentFit="contain" /> */}
     </>
   );
 };
