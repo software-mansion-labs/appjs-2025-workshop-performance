234181afb3f963d40d2b766b8910b18acec628f5
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-05-26 13:09:39 +0200

    Remove container in view reparenting example

--- examples/ViewReparenting/ViewReparenting.tsx
+++ examples/ViewReparenting/ViewReparenting.tsx
@@ -3,23 +3,16 @@ import {StyleSheet, View} from 'react-native';
 
 export default function ViewReparenting() {
   return (
-    <View style={styles.container}>
-      {/* TODO: add collapsable={false} */}
-      <View style={styles.outer}>
-        <View style={styles.middle}>
-          <View style={styles.inner} />
-        </View>
+    // TODO: add collapsable={false}
+    <View style={styles.outer}>
+      <View style={styles.middle}>
+        <View style={styles.inner} />
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   outer: {
     width: 300,
     height: 300,
