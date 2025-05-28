93b6c550118bcf121ea8a6deb4ffc25b847ea340
Tomasz Żelawski <tzelawski@gmail.com>
2025-05-28 11:49:21 +0200

    chore: install React Compiler

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
+const ReactCompilerConfig = {
+  target: '19', // '17' | '18' | '19'
+};
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    ['babel-plugin-react-compiler', ReactCompilerConfig],
     'react-native-worklets/plugin',
     ['babel-plugin-inline-import', {extensions: ['.svg']}],
   ],
